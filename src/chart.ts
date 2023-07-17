352218a5fdef6258e18f0313a6f14033148f16bc
evolveyourmind <20824840+desi-ivanov@users.noreply.github.com>
2023-07-17 20:37:11 +0200

    Add net graph and loss chart

---
+++ src/chart.ts
@@ -0,0 +1,85 @@
+import { MLP } from "./autograd";
+import { distance } from "./gym";
+
+export const drawChart = (canvas: HTMLCanvasElement, values: number[]) => {
+  const ctx = canvas.getContext("2d")!;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  const minDx = 5;
+  values = values.slice(-(canvas.width / minDx));
+  const dx = Math.max(minDx, canvas.width / (values.length - 1));
+  let x = 0;
+  const maxL = Math.max(...values);
+  const minL = Math.min(...values);
+  const norm = (y: number) => (y - minL) / (maxL - minL);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.beginPath();
+  for(const loss of values) {
+    const y = -norm(loss) * canvas.height + canvas.height;
+    ctx.lineTo(x, y);
+    ctx.moveTo(x, y);
+    x += dx;
+  }
+  ctx.stroke();
+  ctx.closePath();
+  ctx.textBaseline = "top"
+  ctx.fillText(maxL.toFixed(4), 0, 0);
+  ctx.textBaseline = "bottom"
+  ctx.fillText(minL.toFixed(4), 0, canvas.height);
+
+}
+
+export const drawGraph = (canvas: HTMLCanvasElement, net: MLP, inputs: number[]) => {
+  const rad = 20;
+  const scaleX = 150;
+  const scaleY = 80;
+  const ctx = canvas.getContext("2d")!;
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  type Node = { x: number, y: number };
+  const nodes: Record<string, Node> = {};
+  const edges: { a: Node, b: Node, w: number }[] = [];
+  for(let i = 0; i < inputs.length; i++) {
+    nodes[`${0},${i}`] = { x: 0, y: i };
+  }
+  for(let i = 0; i < net.layers.length; i++) {
+    for(let j = 0; j < net.layers[i].w.length; j++) {
+      const node: Node = { x: i + 1, y: j };
+      nodes[`${node.x},${node.y}`] = node;
+      for(let k = 0; k < net.layers[i].w[j].w.length; k++) {
+        edges.push({
+          a: node,
+          b: nodes[`${i},${k}`],
+          w: net.layers[i].w[j].w[k].getValue()
+        });
+      }
+    }
+  }
+  const minW = edges.reduce((a, b) => a.w < b.w ? a : b).w;
+  const maxW = edges.reduce((a, b) => a.w > b.w ? a : b).w;
+  const norm = (x: number) => (x - minW) / (maxW - minW);
+  ctx.save();
+  ctx.translate(rad, rad);
+  for(const edge of edges) {
+    ctx.save();;
+    ctx.beginPath();
+    ctx.fillStyle = `rgba(0, 0, 0, ${norm(edge.w)})`
+    const scaledA = { x: edge.a.x * scaleX, y: edge.a.y * scaleY };
+    const scaledB = { x: edge.b.x * scaleX, y: edge.b.y * scaleY };
+    const dif = { x: scaledB.x - scaledA.x, y: scaledB.y - scaledA.y, };
+    ctx.translate(scaledA.x, scaledA.y);
+    ctx.rotate(-Math.atan2(dif.x, dif.y));
+    ctx.rect(0, 0, norm(edge.w) * 10, distance(scaledA, scaledB));
+    ctx.fill();
+    ctx.closePath();
+    ctx.restore();
+  }
+  for(const node of Object.values(nodes)) {
+    ctx.beginPath();
+    ctx.arc(node.x * scaleX, node.y * scaleY, rad, 0, Math.PI * 2);
+    ctx.fillStyle = "#fff";
+    ctx.strokeStyle = "#000";
+    ctx.fill();
+    ctx.stroke();
+    ctx.closePath();
+  }
+  ctx.restore();
+}
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { CarEnv } from "./gym";
 import { Optimizer, RMSProp, MLP, Parameter, reduceSum } from "./autograd";
 import { world } from "./world";
 import { zip } from "./commons";
+import { drawChart, drawGraph } from "./chart";
 
 type Transition = { state: number[], action: number, reward: number, nextState: number[], done: boolean }
 
@@ -26,6 +27,7 @@ const smoothL1 = (yp: Parameter, y: Parameter) => {
 }
 
 const train = (q: MLP, qTarget: MLP, buffer: Transition[], opt: Optimizer) => {
+  const losses = [];
   for(let e = 0; e < EPOCHS; e++) {
     const batch = Array.from({ length: BATCH_SIZE }).map(() => buffer[Math.floor(Math.random() * buffer.length)])
     const preds = batch.map(s => q.forward(s.state)).map((q, i) => q[batch[i].action]);
@@ -34,7 +36,9 @@ const train = (q: MLP, qTarget: MLP, buffer: Transition[], opt: Optimizer) => {
     q.zero_grad();
     loss.backward();
     opt.step();
+    losses.push(loss.getValue());
   }
+  return losses.reduce((a, v) => a + v) / losses.length;
 }
 const getEps = (step: number) => EPS_END + (EPS_START - EPS_END) * Math.exp(-1. * (1.1 ** step) / EPS_DECAY);
 const selectAction = (net: MLP, state: number[], step: number) => {
@@ -43,14 +47,16 @@ const selectAction = (net: MLP, state: number[], step: number) => {
 }
 
 const main = async () => {
-  const canvas = document.getElementById('main-canvas') as HTMLCanvasElement;
+  const gameCanvas = document.getElementById('main-canvas') as HTMLCanvasElement;
+  const netCanvas = document.getElementById('net-canvas') as HTMLCanvasElement;
+  const lossCanvas = document.getElementById('loss-canvas') as HTMLCanvasElement;
   const env = new CarEnv(world, SENSOR_LENGTH, SPEED, STEER_ANGLE);
   const shape = [env.state().length, 5, N_ACTIONS];
   const q = new MLP(shape);
   const qTarget = new MLP(shape);
   const memory: Transition[] = [];
   const optim = new RMSProp(q.parameters(), LR);
-
+  const losses = [];
   for(let nEpisodes = 0; ; nEpisodes++) {
     let game = { state: env.reset(), reward: 0, done: false };
     let score = 0;
@@ -63,12 +69,15 @@ const main = async () => {
 
       score += nextGame.reward;
       document.getElementById('status')!.innerText = `Episode: ${nEpisodes.toString()}, Score: ${score.toFixed(2)}, EPS: ${getEps(nEpisodes).toFixed(2)}, MemSZ: ${memory.length}`;
-      env.render(canvas);
+      env.render(gameCanvas);
       await new Promise(requestAnimationFrame);
     }
     if(memory.length > MIN_TRAIN_MEM) {
-      train(q, qTarget, memory, optim);
+      const loss = train(q, qTarget, memory, optim);
+      losses.push(loss);
     }
+    drawGraph(netCanvas, q, game.state);
+    drawChart(lossCanvas, losses);
     if(nEpisodes % 10 === 0) {
       const bp = q.parameters();
       qTarget.parameters().forEach((p, i) => p.setValue(bp[i].getValue()));
